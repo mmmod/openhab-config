2d613f67b049176a93f2708722bcdbc38b969264
markus <markus.orebrand@gmail.com>
2015-11-22 18:08:06 +0100

    Better install script

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+TARGET_DIR=/etc/openhab/configurations
+
 if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root" 1>&2
 	exit 1
 fi
 
-cp -rv README.md items openhab_default.cfg persistence rules scripts sitemaps transform /etc/openhab/configurations
+# Files
+mkdir -p ${TARGET_DIR}
+
+install -Cv README.md ${TARGET_DIR}
+install -Cv openhab_default.cfg ${TARGET_DIR}
+
+# Directories
+for DIR in items persistence rules scripts sitemaps transform; do
+	mkdir -p ${TARGET_DIR}/${DIR}
+	install -Cv ${DIR}/* ${TARGET_DIR}/${DIR}
+done
